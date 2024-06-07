f6e9c3a56b3d23de4501bc5c4f4ade64babc44d7
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-07 13:33:03 +0100

    fix poc IDAT/IHDRs

--- poc.cpp
+++ poc.cpp
@@ -37,19 +37,28 @@ constexpr auto chunk(const char (&fourcc)[5]) {
 } // namespace frk
 
 struct ihdr {
-  uint32_t width{0x01000000};  // BE
-  uint32_t height{0x01000000}; // BE
-  uint8_t bit_depth{8};
-  uint8_t colour_type{0}; // Grayscale no-alpha
-  uint8_t compression{0}; // Deflate
-  uint8_t filter{0};      // Adaptative
-  uint8_t interlace{0};   // No interlace
+  uint8_t data[13]{
+      0, 0, 0, 1, // Width
+      0, 0, 0, 1, // Height
+      1,          // Bit Depth
+      0,          // Grayscale no-alpha
+      0,          // Deflate
+      0,          // No filter
+      0,          // No interlace
+  };
+};
+static_assert(sizeof(ihdr) == 13);
+
+struct idat {
+  // Single black pixel compressed with zlib/deflate
+  uint8_t data[10]{0x78, 1, 0x63, 0x60, 0, 0, 0, 2, 0, 1};
 };
 
 static void create_file() {
   yoyo::file_writer::open("out/test.png")
       .fmap(frk::signature("PNG"))
       .fmap(frk::chunk("IHDR", ihdr{}))
+      .fmap(frk::chunk("IDAT", idat{}))
       .fmap(frk::chunk("IEND"))
       .map([](auto &&) {})
       .log_error([] { throw 0; });
