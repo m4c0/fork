fb50d7973fd2a6fb5f08bedf510b339dd4c1f186
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-07 16:34:15 +0100

    poc: refactor structs ctors
    otherwise it can give false positives

--- poc.cpp
+++ poc.cpp
@@ -8,21 +8,29 @@ import yoyo;
 using namespace traits::ints;
 
 struct ihdr {
-  uint8_t data[13]{
-      0, 0, 0, 1, // Width
-      0, 0, 0, 1, // Height
-      1,          // Bit Depth
-      0,          // Grayscale no-alpha
-      0,          // Deflate
-      0,          // No filter
-      0,          // No interlace
-  };
+  uint8_t data[13];
+
+  static auto filled() {
+    return ihdr{{
+        0, 0, 0, 1, // Width
+        0, 0, 0, 1, // Height
+        1,          // Bit Depth
+        0,          // Grayscale no-alpha
+        0,          // Deflate
+        0,          // No filter
+        0,          // No interlace
+    }};
+  }
 };
 static_assert(sizeof(ihdr) == 13);
 
 struct idat {
-  // Single black pixel compressed with zlib/deflate
-  uint8_t data[10]{0x78, 1, 0x63, 0x60, 0, 0, 0, 2, 0, 1};
+  uint8_t data[10];
+
+  static auto filled() {
+    // Single black pixel compressed with zlib/deflate
+    return idat{{0x78, 1, 0x63, 0x60, 0, 0, 0, 2, 0, 1}};
+  }
 };
 
 static void do_something_with_ihdr(ihdr h) {
@@ -35,8 +43,8 @@ static void do_something_with_idat(idat h) {
 static void create_file() {
   yoyo::file_writer::open("out/test.png")
       .fmap(frk::signature("PNG"))
-      .fmap(frk::chunk("IHDR", ihdr{}))
-      .fmap(frk::chunk("IDAT", idat{}))
+      .fmap(frk::chunk("IHDR", ihdr::filled()))
+      .fmap(frk::chunk("IDAT", idat::filled()))
       .fmap(frk::chunk("IEND"))
       .map([](auto &&) {})
       .log_error([] { throw 0; });
