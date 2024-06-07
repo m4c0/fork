798e94b8308b6c8e2485f8d2d24173af319829ab
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-07 12:58:20 +0100

    preparing a new api, based on PNG
    The original idea (fourcc + len + data) is the same as used by PNG,
    minus CRC and signature. Fork might gain a big usefullness just with
    that.
    
    Also, the original API required verbose constructions. Simplicity will
    be a certain requirement for the new API.

--- poc.cpp
+++ poc.cpp
@@ -1,41 +1,29 @@
 #pragma leco tool
 import fork;
+import jute;
 import silog;
 import yoyo;
 
-auto write_chunk(yoyo::writer *w, unsigned i) {
-  return frk::push('Chnk', w, i);
+namespace frk {
+constexpr auto signature(const char (&id)[4]) {
+  unsigned char s[]{0x89, 0, 0, 0, 0x0D, 0x0A, 0x1A, 0x0A};
+  for (auto i = 0; i < 3; i++) {
+    s[i + 1] = id[i];
+  }
+
+  return [s](auto &&w) { return w.write(s, sizeof(s)); };
 }
-void create_file() {
-  yoyo::file_writer::open("out/test.dat")
-      .fmap([](auto &&w) {
-        return frk::push('MyDt', &w, [&](auto) {
-          return write_chunk(&w, 10)
-              .fmap([&] { return write_chunk(&w, 30); })
-              .fmap([&] { return write_chunk(&w, 60); });
-        });
-      })
+} // namespace frk
+
+static void create_file() {
+  yoyo::file_writer::open("out/test.png")
+      .fmap(frk::signature("PNG"))
       .log_error([] { throw 0; });
 }
 
-auto read_chunk(frk::pair p) {
-  auto [fourcc, data] = p;
-  return data.read_u32().map([&](auto n) {
-    silog::log(silog::info, "FourCC: %08X - Data: %d", fourcc, n);
-  });
-}
-mno::req<void> read_list(frk::pair p) {
-  auto [fourcc, data] = p;
-  return frk::read_list(&data, read_chunk);
-}
-void read_file() {
-  auto r = yoyo::file_reader::open("out/test.dat").log_error([] { throw 0; });
-  frk::find('MyDt', &r)
-      .fmap([](auto r) { return frk::read_list(&r, read_chunk); })
-      .log_error([] { throw 0; });
-}
-
-int main() {
+int main() try {
   create_file();
-  read_file();
+  return 0;
+} catch (...) {
+  return 1;
 }
